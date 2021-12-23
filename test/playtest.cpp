15dbc47da05e11e40edb81a321f402c8cb9dd7c6
hideyosh1 <64223175+hideyosh1@users.noreply.github.com>
2021-12-23 15:27:50 -0600

    level generation algorithm

--- test/playtest.cpp
+++ test/playtest.cpp
@@ -1,29 +1,64 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
+//the algorithm now:
+//start a new map with random size (constrained by pad size)
+//set start point at random place
+//for the "difficulty level" determined by the level number:
+	//set a random direction (up, down, left right)
+	//set a random moving distance
+	//move in the direction for the moving distance
+	//if it collides with a previous path restart from the last valid direction.
+	//if it collides with the level boundaries don't restart and just break moving and start anew.
+//at the final step mark it as '3'.
 #include <random>
+#include <vector>
 
-int main(){
-	//pick a random value between 3 and 14 and 3 and 10 for x and y
-	//then fill in the whole screen with void
-	//place the player coordinate at the top left
-	//the rule is that every tile must have two adjacent tiles otherwise it's impossible for the player to move
-	std::default_random_engine nice;
-	std::uniform_int_distribution<int> disty(3, 14);
-	std::uniform_int_distribution<int> distx(3, 10);
-	int mapy = disty(nice);
-	int mapx = distx(nice);
-
+std::vector<std::string> default_levelgen(int lvl){
 	std::vector<std::string> map;
-	map.resize(mapy, "");
-	for(int i = 0; i < mapy; i++){
-		for(int j = 0; j < mapx; j++){
-			map[i].push_back('4');
+	std::random_device seed;
+	std::mt19937 gen(seed());
+	
+	std::uniform_int_distribution<> ydist(1, 8);
+	std::uniform_int_distribution<> xdist(1, 10);
+	std::uniform_int_distribution<> direction(1, 4);
+	std::uniform_int_distribution<> walklength(1, 7);
+	
+	int y = ydist(gen);
+	int x = xdist(gen);
+	
+	const int starty = ydist(gen);
+	const int startx = xdist(gen);
+	
+	int cursy = starty;
+	int cursx = startx;
+	
+	for(int i = 0; i < lvl % 5 + 5; i++){
+		int dir = direction(gen);
+		int walkl = walklength(gen);
+		
+		for(int j = 0; j < walkl; j++){
+			switch(dir){
+				case 1:
+					//up
+					cursy--;
+					break;
+				case 2:
+					//down
+					cursy++;
+					break;
+				case 3:
+					//left
+					cursx--;
+					break;
+				case 4:
+					//right
+					cursx++;
+					break;
+				default:
+					//up
+					cursy--;
+					break;
+					
+			}
 		}
 	}
-	std::ofstream out("out.txt");
-	for(int i = 0; i < map.size(); i++){
-		out << map[i] << "\n";
-	}
-	out.close();
+	
 }
