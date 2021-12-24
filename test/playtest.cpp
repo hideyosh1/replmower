36ced130f9ec12d2486d3d8b424ba386a2d3b29c
hideyoshi5823 <hideyoshi5823@outlook.com>
2021-12-24 10:10:57 -0600

    chunktest

--- test/playtest.cpp
+++ test/playtest.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <fstream>
 #include <vector>
 #include <random>
@@ -26,4 +26,89 @@ int main(){
 		out << map[i] << "\n";
 	}
 	out.close();
+}*/
+
+#include <iostream>
+#include <vector>
+#include <random>
+int main(){ //this is an idea on how tog enerate a map.
+//111
+//441
+//1111
+//maps should be "chunky": no separated floating parts are allowed. so we can define
+//several "chunk" variants like 
+//44
+//44 and
+//44
+//41 and go from there.
+
+//chunks
+/*
+44
+41
+
+44
+44
+
+14
+44
+
+41
+44 etc
+*/
+
+	std::vector<std::string> map; //level number determines the map size
+	std::vector<std::string> chunk;
+
+	std::default_random_engine rand;
+	std::uniform_int_distribution<int> distribution(1, 14);
+	switch(distribution(rand)){
+		case 1:
+			chunk = {"44", "44"};
+			break;
+		case 2:
+			chunk = {"44", "41"};
+			break;
+		case 3:
+			chunk = {"44", "14"};
+			break;
+		case 4:
+			chunk = {"14", "44"};
+			break;
+		case 5:
+			chunk = {"41", "44"};
+			break;
+		case 6:
+			chunk = {"11", "44"};
+			break;
+		case 7:
+			chunk = {"44", "11"};
+			break;
+		case 8:
+			chunk = {"14", "14"};
+			break;
+		case 9:
+			chunk = {"41", "41"};
+			break;
+		case 10:
+			chunk = {"11", "14"};
+			break;
+		case 11:
+			chunk = {"11", "41"};
+			break;
+		case 12:
+			chunk = {"14", "11"};
+			break;
+		case 13:
+			chunk = {"41", "11"};
+			break;
+		case 14:
+			chunk = {"11, 11"};
+			break;
+		default:
+			chunk = {"44", "44"};
+	}
+	std::cout << chunk[0] << chunk[1];
+	char nice;
+	std::cin >> nice;
 }
